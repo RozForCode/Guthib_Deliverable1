fc3f0144179221b15043b72741ee42fc5dfe3ee3
Navrose Johal <johnavro@sheridancollege.ca>
2025-02-13 10:05:54 -0500

    Initialized project

---
+++ Deliverable1/src/warGame/Card.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package warGame;
+
+/**
+ *
+ * @author navro
+ */
+public class Card {
+    private String suit;
+    private String rank;
+
+    public Card(String suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
+
